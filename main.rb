d197f26573bf2c7aa5441657e9f472dd7268bfbc
shikibum <satoshikibu@gmail.com>
2018-08-14 13:55:40 +0900

    クラス名を付与

--- main.rb
+++ main.rb
@@ -10,9 +10,9 @@ set :cookie_options, { domain: 'localhost', path: '/' }
 
 get '/' do
   @contents = Dir.glob('files/*').map do |name|
-    open(name) {|f|
+    open(name) do |f|
       JSON.parse(f.read)
-    }
+    end
   end
 
   erb :index
@@ -26,13 +26,13 @@ post '/memos' do
   id = SecureRandom.hex(16)
   while File.exist?("files/#{id}")
     id = SecureRandom.hex(16)
-  end 
+  end
 
-  open("files/#{id}", 'w') {|f|
+  open("files/#{id}", 'w') do |f|
     a = params.slice('title', 'body')
     a.merge('id' => id, 'time' => Time.now)
     f.write(a.to_json)
-  }
+  end
   redirect "/memos/#{id}"
 end
 
@@ -42,31 +42,31 @@ get '/memos/:id/edit' do
   end
 
   if File.exist?("locks/#{params['id']}")
-    user_id = open("locks/#{params['id']}") {|f|
+    user_id = open("locks/#{params['id']}") do |f|
       f.read
-    }
+    end
     if user_id != cookies[:user_id]
       redirect '/error'
       return
     end
   end
 
-  open("locks/#{params['id']}", 'w'){|f|
+  open("locks/#{params['id']}", 'w') do |f|
     f.write(cookies[:user_id])
-  }
-  @content = open("files/#{params['id']}") {|f|
+  end
+  @content = open("files/#{params['id']}") do |f|
     JSON.parse(f.read)
-  }
-  
+  end
+
   erb :edit
 end
 
 patch '/memos/:id' do
-  open("files/#{params['id']}", 'w') {|f|
+  open("files/#{params['id']}", 'w') do |f|
     a = params.slice('title', 'body', 'id', 'time')
     a['time'] = Time.now
     f.write(a.to_json)
-  }
+  end
   File.delete("locks/#{params['id']}")
   redirect "/memos/#{params['id']}"
 end
@@ -77,9 +77,9 @@ patch '/memos/:id/cancel' do
 end
 
 get '/memos/:id' do
-  @content = open("files/#{params['id']}") {|f|
+  @content = open("files/#{params['id']}") do |f|
     JSON.parse(f.read)
-  }
+  end
 
   erb :show
 end
@@ -91,4 +91,4 @@ end
 
 get '/error' do
   erb :error
-end 
+end
